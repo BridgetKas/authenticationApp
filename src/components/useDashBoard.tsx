366d0b6e1d9739a183bb8a179f01630b76ad30f2
BridgetKas <bridgetnamugga@gmail.com>
2024-11-29 20:28:07 +0300

    Modified dashboard component

--- src/components/useDashBoard.tsx
+++ src/components/useDashBoard.tsx
@@ -5,27 +5,29 @@ import { MdOutlineDashboard } from "react-icons/md";
 
 function UserDashBoard() {
   return (
-    <div>
+    <div className="flex items-center  gap-4 w-[80%] mx-auto mt-[80px]">
         <div>
-            <div>
+            <div className="flex items-center gap-2.5 mb-3">
                 <MdOutlineDashboard />
                 <p>Overview</p>
             </div>
-            <div>
+            <div className="flex items-center gap-2.5 mb-3">
                 <FaRegCalendarCheck />
                 <p>Calendar</p>
             </div>
-            <div>
+            <div className="flex items-center gap-2.5 mb-3">
                 <MdEmail />
                 <p>Messgaes</p>
             </div>
-            <div>
+            <div className="flex items-center gap-2.5 mb-3">
                 <AiTwotoneContacts />
                 <p>Contacts</p>
             </div>
         </div>
-        <div>
-
+        <div >
+            <div className="flex  flex-row items-center justify-center gap-5">
+                <p className="text-2xl">Welcome user</p>
+            </div>
         </div>
     </div>
   )
